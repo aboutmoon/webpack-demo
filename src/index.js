c7dc541cd768372f515535a055efa33c03dac409
biao.he@kaiostech.com <biao.he@kaiostech.com>
2019-08-22 18:51:14 +0800

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
     const element = document.createElement('div');
 
+    const btn = document.createElement("button");
+
     element.innerHTML = _.join(['你好', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
